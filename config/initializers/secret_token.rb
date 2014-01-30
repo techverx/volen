7d01332fe8e9c0352889258878e43aa006cd4e71
Usman Khan <usman@ubuntu.(none)>
2014-01-31 03:00:56 +0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Volen::Application.config.secret_key_base = '96b54f2ea7f0db944d0420dec4b954ab8fe8307c0f2283916fe4cfd6249c18bcf4248c60a63d54b6ce59c4040859b4f054429daa13c8f9471be430db75cb5fd7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Volen::Application.config.session_store :cookie_store, key: '_volen_session'
